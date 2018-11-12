019b9dfb15cb0db8b29b5b7dcc295ce8f8235d43
joaquin cuneo <joaquincuneo@hotmail.com>
2018-11-12 10:25:16 +0200

    1,2

---
+++ ex6/DNA/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindSubSequence("atgcttcggcaagactc", "hbv"));
+        }
+
+        static bool FindSubSequence (string sequence, string sub)
+        {
+            for(int i=0;i<= sequence.Length-sub.Length;i++)
+            {
+                if(sequence.Substring(i,sub.Length)==sub)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ ex6/countLetter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace countLetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountLetter("hello world", 'l'));
+
+        }
+
+        static int CountLetter(string sentence, char letter)
+        {
+            int counter = 0;
+            int len = sentence.Length;
+            for (int i = 0; i < len; i++)
+            {
+                if (sentence[i]==letter)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+    }
+}
