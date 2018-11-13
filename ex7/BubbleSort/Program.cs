73236a8c2d542bebe2ee66ecff99a5adf9162f21
joaquin cuneo <joaquincuneo@hotmail.com>
2018-11-13 12:28:14 +0200

    update4

---
+++ ex7/BubbleSort/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 2, 9, 5, 1, 4, 7, 3, 8 };
+            PrintArr(nums);
+            BubbleSort(nums);
+            PrintArr(nums);
+        }
+
+
+        static void BubbleSort(int[] nums)
+        {
+
+            int temp;
+            for (int i = 0; i <= nums.Length; i++)
+            {
+                for (int j = 0; j <= nums.Length - i; j++)
+                {
+                    if (nums[j + 1] < nums[j])
+                    {
+                        temp = nums[j];
+                        nums[j] = nums[j + 1];
+                        nums[j + 1] = temp;
+                    }
+                }
+            }
+        }
+        static void PrintArr(int[] arr)
+        {
+            Console.Write("\nArray Values:");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i] + ", ");
+            }
+            Console.WriteLine("\b\b");
+        }
+    }
+}
+
+
--- ex7/Even/Program.cs
+++ ex7/Even/Program.cs
@@ -11,25 +11,24 @@ namespace Even
         static void Main(string[] args)
         {
             PrintArray(FilterEvens(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }));
-            
         }
 
-        static void PrintArray(int[]nums)
+        static void PrintArray(int[] nums)
         {
             Console.WriteLine("\nArray:");
-            for(int i=0;i<nums.Length;i++)
+            for (int i = 0; i < nums.Length; i++)
             {
                 Console.WriteLine(nums[i] + ", ");
             }
             Console.WriteLine();
         }
 
-        static int [] FilterEvens(int [] numbers)
+        static int[] FilterEvens(int[] numbers)
         {
-          int count = 0;
-          for (int i=0;i<numbers.Length;i++)
+            int count = 0;
+            for (int i = 0; i < numbers.Length; i++)
             {
-                if(numbers[i]%2==0)
+                if (numbers[i] % 2 == 0)
                 {
                     count++;
                 }
@@ -37,9 +36,9 @@ namespace Even
             int[] evenNums = new int[count];
 
 
-            for (int i=0,j=0;i<numbers.Length;i++)
+            for (int i = 0, j = 0; i < numbers.Length; i++)
             {
-                if(numbers[i]%2==0)
+                if (numbers[i] % 2 == 0)
                 {
                     evenNums[j++] = numbers[i];
                 }
